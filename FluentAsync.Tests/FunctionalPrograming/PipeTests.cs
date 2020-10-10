9063c4997107501b9330d835cfd3f2e2f45cff71
Pierre GILLON <contact@pierregillon.com>
2020-10-10 14:09:37 +0200

    feat: implement pipe and pipeasync

---
+++ FluentAsync.Tests/FunctionalPrograming/PipeTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace FluentAsync.Tests.FunctionalPrograming
+{
+    public class PipeTests
+    {
+        private static double Pow2(double x) => Math.Pow(x, 2);
+        private static double Add1(double x) => x + 1;
+
+        [Fact]
+        public void Pipe_functions()
+        {
+            var result = 10d
+                .Pipe(Pow2)
+                .Pipe(Add1);
+
+            result
+                .Should()
+                .Be(101);
+        }
+
+        [Fact]
+        public async Task Asynchronously_pipe_functions()
+        {
+            var result = await Task.FromResult(10d)
+                .PipeAsync(Pow2)
+                .PipeAsync(Add1);
+
+            result
+                .Should()
+                .Be(101);
+        }
+    }
+}
---
+++ FluentAsync/FunctionalProgramingExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FluentAsync
+{
+    public static class FunctionalProgramingExtensions
+    {
+        public static TResult Pipe<T, TResult>(this T element, Func<T, TResult> projection) => projection(element);
+
+        public static async Task<TResult> PipeAsync<T, TResult>(this Task<T> element, Func<T, TResult> projection) => projection(await element);
+    }
+}
