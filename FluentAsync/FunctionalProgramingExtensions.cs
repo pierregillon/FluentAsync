55fdd357049c3c8b71592eec6e055fec246a36d8
Pierre GILLON <contact@pierregillon.com>
2020-10-11 12:07:31 +0200

    feat: add PipeAsync() extension for asynchronous delegate.

--- FluentAsync/FunctionalProgramingExtensions.cs
+++ FluentAsync/FunctionalProgramingExtensions.cs
@@ -10,17 +10,19 @@ namespace FluentAsync
         /// <summary>
         /// Apply a projection function on the current element.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>The result of the function called with the caller.</returns>
         public static TResult Pipe<T, TResult>(this T element, Func<T, TResult> projection) => projection(element);
 
         /// <summary>
         /// Apply a projection function on a task result.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <typeparam name="TResult"></typeparam>
-        /// <param name="element"></param>
-        /// <param name="projection"></param>
-        /// <returns></returns>
+        /// <returns>The result of the function called with the caller.</returns>
         public static async Task<TResult> PipeAsync<T, TResult>(this Task<T> element, Func<T, TResult> projection) => projection(await element);
+
+        /// <summary>
+        /// Apply an asynchronous function on a task result.
+        /// </summary>
+        /// <returns>The result of the function called with the caller.</returns>
+        public static async Task<TResult> PipeAsync<T, TResult>(this Task<T> element, Func<T, Task<TResult>> action) => await action(await element);
     }
 }
