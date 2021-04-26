47565e49df968707ec996c666a2825ee731ce0d7
Pierre GILLON <pierre.gillon@agicap.fr>
2021-04-26 08:45:02 +0200

    tests: add tests on FirstAsync() and OrderByAsync()

---
+++ FluentAsync.Tests/Tasks/FirstAsyncTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using FluentAsync.Tests.Utils;
+using Xunit;
+
+namespace FluentAsync.Tests.Tasks
+{
+    public class FirstAsyncTests
+    {
+        private static readonly IEnumerable<string> Elements = new List<string> {
+            "hello world",
+            "please",
+            "do it now",
+            "cuz"
+        };
+
+        private readonly ITask<IEnumerable<string>> task = Task.FromResult(Elements).ToCovariantTask();
+
+        [Fact]
+        public async Task Get_the_first_element_of_an_enumerable_task()
+        {
+            (await task.FirstAsync()).Should().BeEquivalentTo("hello world");
+            (await task.FirstOrDefaultAsync()).Should().BeEquivalentTo("hello world");
+        }
+
+        [Fact]
+        public void Throw_error_when_getting_the_first_element_of_an_empty_enumerable_task()
+        {
+            var emptyTask = TaskUtils.WaitAndReturn(new List<string>()).ToCovariantTask();
+
+            Func<Task> gettingFirst = async () => await emptyTask.FirstAsync();
+
+            gettingFirst.Should().Throw<InvalidOperationException>().WithMessage("Sequence contains no elements");
+        }
+
+        [Fact]
+        public async Task Get_the_first_element_of_an_enumerable_task_matching_a_predicate()
+        {
+            static bool Predicate(string x) => !x.Contains(" ");
+
+            (await task.FirstAsync(Predicate)).Should().BeEquivalentTo("please");
+            (await task.FirstOrDefaultAsync(Predicate)).Should().BeEquivalentTo("please");
+        }
+
+        [Fact]
+        public async Task Returns_default_element_on_empty_enumerable()
+        {
+            var emptyTask = TaskUtils.WaitAndReturn(new List<string>()).ToCovariantTask();
+
+            var result = await emptyTask.FirstOrDefaultAsync();
+
+            result.Should().Be(default);
+        }
+    }
+}
---
+++ FluentAsync.Tests/Tasks/OrderByAsyncTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace FluentAsync.Tests.Tasks
+{
+    public class OrderByAsyncTests
+    {
+        private static readonly IEnumerable<string> Elements = new List<string> {
+            "hello world",
+            "please",
+            "do it now",
+            "cuz"
+        };
+
+        private readonly ITask<IEnumerable<string>> task = Task.FromResult(Elements).ToCovariantTask();
+
+        [Fact]
+        public async Task Asynchronously_order_ascending_elements()
+        {
+            var expectedOrder = new [] { "cuz", "do it now", "hello world", "please" };
+
+            (await task.OrderByAsync()).Should().ContainInOrder(expectedOrder);
+            (await task.OrderByAsync(x => x)).Should().ContainInOrder(expectedOrder);
+        }
+
+        [Fact]
+        public async Task Asynchronously_order_ascending_elements_by_selector()
+        {
+            (await task.OrderByAsync(x => x.Length)).Should().ContainInOrder("cuz", "please", "do it now", "hello world");
+        }
+
+        [Fact]
+        public async Task Asynchronously_order_descending_elements()
+        {
+            var expectedOrder = new [] { "please", "hello world", "do it now", "cuz" };
+            
+            (await task.OrderByDescendingAsync()).Should().ContainInOrder(expectedOrder);
+            (await task.OrderByDescendingAsync(x => x)).Should().ContainInOrder(expectedOrder);
+        }
+
+        [Fact]
+        public async Task Asynchronously_order_descending_elements_by_selector()
+        {
+            (await task.OrderByDescendingAsync(x => x.Length)).Should().ContainInOrder("hello world", "do it now", "please", "cuz");
+        }
+    }
+}
